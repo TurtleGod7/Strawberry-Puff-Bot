eb7cc4d259bc2ae06d66dcdd403f8de507f1e437
TurtleGod7 <157254562+TurtleGod7@users.noreply.github.com>
2025-02-17 23:09:56 -0600

    Changed pity mechanics, added ascencsions, made things cleaner

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import os
 import discord
-from flask import Flask
-import threading
 from discord.ext import commands
 from dotenv import load_dotenv
-import random
+from random import choices
 import sqlite3
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-# Initialize Flask and the bot
-app = Flask(__name__)
-
-## Control variables
+### Control variables
 pity_limit = 100
 git_username = "TurtleGod7"
 git_repo = "Strawberry-Puff-Bot"
+button_page_expiry = 60
+items_per_page = 5
+eidolon_max = 6
+###
+
 '''
 Gold Rarity puffs: <1%
 Purple rarity puffs: 1%<=x<=10%
 Blue rarity puffs: everything else
 '''
+
+'''
+from flask import Flask
+import threading
+
+# Initialize Flask and the bot
+app = Flask(__name__)
+
 # Define a simple route for UptimeRobot
 @app.route('/')
 def index():
@@ -34,6 +42,7 @@ def run_flask():
 # Start Flask in a separate thread to avoid blocking the bot
 thread = threading.Thread(target=run_flask)
 thread.start()
+'''
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -53,9 +62,12 @@ async def on_ready():
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         description TEXT NOT NULL,
-        imagepath TEXT NOT NULL
+        imagepath TEXT NOT NULL UNIQUE,
+        weight INTEGER,
+        isRare NUMERIC NOT NULL DEFAULT 0
     )
     """)
+    cursor.close
     conn.close
     
     conn = sqlite3.connect("assets\\database\\users.db") if os.name == "nt" else sqlite3.connect("assets/database/users.db")
@@ -64,15 +76,23 @@ async def on_ready():
     
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS stats (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL UNIQUE,
-        rolls INTEGER,
-        rare rolls INTEGER
+        "username" INTEGER PRIMARY KEY NOT NULL UNIQUE,
+        "rolls" INTEGER DEFAULT 0,
+        "gold" rolls INTEGER DEFAULT 0,
+        "purple" INTEGER DEFAULT 0,
+        "rolledGolds" TEXT
     )               
     """)
     
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pity (
+        "username" INTEGER PRIMARY KEY NOT NULL UNIQUE,
+        "pity" INTEGER NOT NULL DEFAULT 0
+    )               
+    """)
     cursor.close()
     conn.close()
+    
     print(f'Logged in as {bot.user}')    
 
 @bot.tree.command(name="puffroll", description="Roll a random puff")
@@ -104,7 +124,7 @@ async def Roll_a_puff(interaction: discord.Interaction):
         cursor.execute("SELECT id, weight FROM puffs")
         data =  cursor.fetchall()
         items, weights = zip(*data) # Randomly selects a weighted role (id)
-        selected_id = random.choices(items, weights=weights, k=1)[0]
+        selected_id = choices(items, weights=weights, k=1)[0]
         cursor.execute("SELECT * FROM puffs WHERE id = ?", (selected_id,))
         choice = cursor.fetchone() # Gets the full info from the id 
         
@@ -113,21 +133,21 @@ async def Roll_a_puff(interaction: discord.Interaction):
         
         cursor.close() # Gets info for the chance calculation
         conn.close()
-        
-        item_id, name, description, image_path, weights, isRare = choice
-
     else:
-        cursor.execute("SELECT * FROM puffs WHERE isRare = 2")
-        rows = cursor.fetchall()
-        choice = random.choice(rows)
+        cursor.execute("SELECT id, weight FROM puffs WHERE isRare = 2")
+        data =  cursor.fetchall()
+        items, weights = zip(*data) # Randomly selects a weighted role (id)
+        selected_id = choices(items, weights=weights, k=1)[0]
+        cursor.execute("SELECT * FROM puffs WHERE id = ?", (selected_id,))
+        choice = cursor.fetchone() # Gets the full info from the id 
         
-        total_weight = len(rows)
-        weights = 1
-        
+        cursor.execute("SELECT SUM(weight) FROM puffs WHERE isRare = 2")
+        total_weight = cursor.fetchone()[0]
         cursor.close()
+        
         conn.close()
         
-        item_id, name, description, image_path, notneededweight, isRare = choice
+    item_id, name, description, image_path, weights, isRare = choice
     
     chance = round(round(weights/int(total_weight), 4)*100,2)
     
@@ -135,10 +155,22 @@ async def Roll_a_puff(interaction: discord.Interaction):
     
     cursor = conn.cursor()
     
-    
     cursor.execute("SELECT EXISTS(SELECT 1 FROM stats WHERE username = ?)", (user_id,))
     if cursor.fetchone()[0] == 0: 
-        cursor.execute("INSERT INTO stats (username, rolls, gold, purple) VALUES (?,?,?,?)", (user_id, 0, 0, 0))
+        cursor.execute("INSERT INTO stats (username, rolls, gold, purple, rolledGolds) VALUES (?,?,?,?,?)", (user_id, 0, 0, 0,""))
+    
+    cursor.execute("SELECT rolledGolds FROM stats WHERE username = ?", (user_id,))
+    rolledGolds = cursor.fetchone()[0]
+    frequency = {}
+    if None != rolledGolds:
+        split_by_puffs = rolledGolds.split(";")
+        for split in split_by_puffs:
+            frequency[split.split("_")[0]] = int(split.split("_")[1])
+    
+    if isRare == 2:
+        eidolon = frequency.get(name, -1)
+        if eidolon < eidolon_max:
+            frequency[name] = eidolon+1
     
     cursor.execute("UPDATE stats SET rolls = rolls + 1 WHERE username = ?", (user_id,))
     
@@ -151,6 +183,8 @@ async def Roll_a_puff(interaction: discord.Interaction):
 
     if isRare == 2:
         cursor.execute("UPDATE pity SET pity = 0 WHERE username = ?", (user_id,))
+    
+    cursor.execute("UPDATE stats SET rolledGolds = ? WHERE username = ?", (";".join([f"{k}_{v}" for k, v in frequency.items()]) or None, user_id))
     
     conn.commit()
     cursor.close()
@@ -168,21 +202,12 @@ async def Roll_a_puff(interaction: discord.Interaction):
     if isRare == 2:
         embed.add_field(
             name=":strawberry::turtle::strawberry::turtle::strawberry::turtle::strawberry::turtle::strawberry:",
-            value=f"""
-            You got a **{name}**.\n
-            It is {description}\n
-            It was a **{chance}%** chance to roll this puff!\n
-            You rolled this puff at **{pity}** pity.\n
-            """
+            value=f"You got a **{name}**.\nIt is {description}\nIt was a **{chance}%** chance to roll this puff!\nYou rolled this puff at **{pity}** pity.\n"
         )
     else:
         embed.add_field(
             name=":strawberry::turtle::strawberry::turtle::strawberry::turtle::strawberry::turtle::strawberry:",
-            value=f"""
-            You got a **{name}**.\n
-            It is {description}\n
-            It was a **{chance}%** chance to roll this puff!\n
-            """
+            value=f"You got a **{name}**.\nIt is {description}\nIt was a **{chance}%** chance to roll this puff!\n"
         )
     embed.set_image(url=image_path)
     embed.set_footer(text=f"Requested by {interaction.user.display_name}")
@@ -190,7 +215,7 @@ async def Roll_a_puff(interaction: discord.Interaction):
     await interaction.response.send_message(embed=embed)
 
 @bot.tree.command(name="statistics", description="Get some info on your rolls")
-async def stats(interaction: discord.Interaction):
+async def statistics(interaction: discord.Interaction):
     conn = sqlite3.connect("assets\\database\\users.db") if os.name == "nt" else sqlite3.connect("assets/database/users.db")
     
     cursor = conn.cursor()
@@ -199,25 +224,43 @@ async def stats(interaction: discord.Interaction):
     
     cursor.execute("SELECT * FROM stats WHERE username = ?", (user_id,))
     choice = cursor.fetchone()
-    username, rolls, gold, purple = choice
+    
+    if choice is None:
+        cursor.execute("INSERT INTO stats (username, rolls, gold, purple, rolledGolds) VALUES (?, ?, ?, ?)", (user_id, 0, 0, 0,""))
+        conn.commit()
+    
+    username, rolls, gold, purple, rolledGolds = choice
     
     cursor.close()
     conn.close()
     
+    frequency = {}
+    if None != rolledGolds:
+        split_by_puffs = rolledGolds.split(";")
+        for split in split_by_puffs:
+            frequency[split.split("_")[0]] = int(split.split("_")[1])
+    
+    ascenscions_description_string = ""
+    for k, v in frequency.items():
+        ascenscions_description_string += f"* *{k}*  **{v}** time(s)\n"
+    if ascenscions_description_string == "":
+        ascenscions_description_string += "You're seeing this because you didn't pull any gold rarity puffs :sob:"
+    
     embed = discord.Embed(title="Your Puff Gacha statistics", color=discord.Color.blurple())
     embed.add_field(name="Total Rolls", value=f"You've rolled **{rolls}** times!", inline=False)
     embed.add_field(name="Rare Rolls", value=f"You've also ~~pulled~~ rolled a gold rarity puff **{gold}** times and a purple rarity puff **{purple}** times!", inline=False)
+    embed.add_field(name="Ascensions", value=ascenscions_description_string, inline=False)
     embed.set_footer(text=f"Requested by {interaction.user.display_name}")
     
     await interaction.response.send_message(embed=embed)
 
 class DropRatesView(discord.ui.View):
     def __init__(self, items, total_weight):
-        super().__init__(timeout=60)  # Buttons expire after 60 seconds
+        super().__init__(timeout=button_page_expiry)  # Buttons expire after 60 seconds
         self.items = items
         self.total_weight = total_weight
         self.page = 0
-        self.items_per_page = 5  # Adjust if needed
+        self.items_per_page = items_per_page  # Adjust if needed
 
     def generate_embed(self):
         embed = discord.Embed(title="📊 Puff Drop Rates", color=discord.Color.gold())
@@ -274,7 +317,7 @@ async def stats(interaction: discord.Interaction):
     embed.add_field(name="Please redirect your suggestions to this google form", value="*https://forms.gle/gce7woXR5i38fnXY7*")
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
-@bot.tree.command(name="help")
+@bot.tree.command(name="help", description="AHHHHH, I NEED HELP!!!!")
 async def help(interaction: discord.Interaction):
     embed = discord.Embed(title="Techsupport is on the way!", color=discord.Color.greyple())
     embed.add_field(name="/puffroll", value="This is the major mechanic of this bot and this is how you set up your local account.")
@@ -285,4 +328,23 @@ async def help(interaction: discord.Interaction):
     
     await interaction.response.send_message(embed=embed)
 
-bot.run(TOKEN)
+@bot.tree.command(name="info", description="Just some good to know information")
+async def information(interaction: discord.Interaction):
+    embed = discord.Embed(title="Good to know information", color=discord.Color.dark_orange())
+    embed.add_field(
+        name="Rarities", 
+        value="1. :yellow_square: is a gold rarity puff (the highest rarity available)\u200b\n2. :purple_square: is a purple rarity puff that is the second rarest puff to get\u200b\n3. Finally a :blue_square: is a blue rarity puff that is the most common type to get\nPlease check the /chances function to see what they corrolate to", 
+        inline=False
+    )
+    embed.add_field(
+        name="How is information saved?",
+        value="Information like\n* amount of rolls\n* pity\n* types of rolls\nare **NOT** server specifc (AKA Discord-wide)\nThis means that lets say you roll a puff in another server, this will affect your experience in this server", 
+        inline=False
+    )
+    
+    embed.set_footer(text=f"Requested by {interaction.user.display_name}")
+    
+    await interaction.response.send_message(embed=embed)
+
+bot.run(TOKEN) # type: ignore
+
